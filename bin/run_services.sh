c98f300451afea7e99ff876c996e392628580a8f
adestaff <augustin.destaff@gmail.com>
2024-10-24 23:40:37 +0200

    final version of Dockerfile.app and run_services.sh - API and Prefect both runnning when running docker container

--- bin/run_services.sh
+++ bin/run_services.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Set Prefect API URL to be accessible at 0.0.0.0:4200
-prefect config set PREFECT_API_URL=http://127.0.0.1:4200/api
+# Set Prefect API URL to be accessible at 0.0.0.0:4201
+prefect config set PREFECT_API_URL=http://0.0.0.0:4200/api
 
 # Start Prefect server in the background
-prefect server start --host 127.0.0.1 --port 4200 &
+prefect server start --host 0.0.0.0 --port 4200 &
 
 # Navigate to the correct directory
 cd /src/web_service  # Make sure this path is correct
 
-# Start FastAPI with Uvicorn on port 8001
-uvicorn main:app --host 127.0.0.1 --port 8001 &
+# Start FastAPI with Uvicorn on port 8000
+uvicorn main:app --host 0.0.0.0 --port 8000 &
 
 # Wait for Prefect server to be live before running deployment.py
 sleep 10
