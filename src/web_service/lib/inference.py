5266ed5bc3393a2863356185e20a9fa87f639902
Matthieu Delsart <matthieu.delsart@hec.edu>
2024-10-24 16:27:11 +0200

    fix: inference.py

--- src/web_service/lib/inference.py
+++ src/web_service/lib/inference.py
@@ -3,14 +3,19 @@ from typing import List
 import numpy as np
 import pandas as pd
 from loguru import logger
+from models import InputData
 from sklearn.base import BaseEstimator
 from sklearn.compose import ColumnTransformer
 
-from models import InputData
-from ..modelling.preprocessing import compute_target, fit_preprocessor, extract_x_y, preprocess_data
+from ...modelling.predicting import predict
+from ...modelling.preprocessing import preprocess_data
 
 
-def run_inference(input_data: List[InputData], preprocessor: ColumnTransformer, model: BaseEstimator) -> np.ndarray:
+def run_inference(
+    input_data: List[InputData],
+    preprocessor: ColumnTransformer,
+    model: BaseEstimator,
+) -> np.ndarray:
     """Run inference on a list of input data.
 
     Args:
@@ -21,9 +26,12 @@ def run_inference(input_data: List[InputData], preprocessor: ColumnTransformer, 
     Returns:
         np.ndarray: the predicted abalone ages in years.
     """
+
     logger.info(f"Running inference on:\n{input_data}")
     df = pd.DataFrame([x.dict() for x in input_data])
-    x, _, _ = preprocess_data(df, is_train=False, preprocessor=preprocessor, with_target=False)
+    x, _, _ = preprocess_data(
+        df, is_train=False, preprocessor=preprocessor, with_target=False
+    )
     y = predict(x, model)
-    logger.info(f"Predicted trip durations:\n{y}")
+    logger.info(f"Predicted abalone ages:\n{y}")
     return y
