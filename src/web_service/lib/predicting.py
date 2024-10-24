e68c90679859c5199d6740cc0a62c242910f4605
Matthieu Delsart <matthieu.delsart@hec.edu>
2024-10-24 22:23:02 +0200

    fix: clean code + improve readme

--- src/web_service/lib/predicting.py
+++ src/web_service/lib/predicting.py
@@ -3,5 +3,16 @@ from sklearn.linear_model import Ridge
 
 
 def predict(X: np.ndarray, model: Ridge) -> np.ndarray:
-    """Make predictions using the model."""
+    """
+    Make predictions on input data using a pre-trained Ridge regression model.
+
+    Parameters:
+        X (np.ndarray): A 2D NumPy array of input features where each row represents
+                        a data sample and each column represents a feature.
+        model (Ridge): A pre-trained Ridge regression model used to make predictions.
+
+    Returns:
+        np.ndarray: A 1D NumPy array of predicted values corresponding
+        to the input data.
+    """
     return model.predict(X)
