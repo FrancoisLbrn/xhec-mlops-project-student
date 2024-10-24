c6cb247c664408e84ca5507b981db34afae48e3d
FrancoisLbrn <francois.lebrun3@gmail.com>
2024-10-24 14:18:51 +0200

    feat: Create the API

--- src/web_service/main.py
+++ src/web_service/main.py
@@ -1,18 +1,20 @@
-# Code with FastAPI (app = FastAPI(...))
-
-
+from app_config import APP_DESCRIPTION, APP_TITLE, APP_VERSION, MODEL_VERSION
 from fastapi import FastAPI
+from lib.models import InputData, PredictionOut
 
-# Other imports
-
-app = FastAPI(title="...", description="...")
+app = FastAPI(
+    title=APP_TITLE, description=APP_DESCRIPTION, version=APP_VERSION
+)
 
 
 @app.get("/")
 def home() -> dict:
-    return {"health_check": "App up and running!"}
+    return {
+        "health_check": "App up and running!",
+        "model_version": MODEL_VERSION,
+    }
 
 
-@app.post("/predict", response_model="InsertHereAPydanticClass", status_code=201)
-def predict(payload: "InsertHereAPydanticClass") -> dict:
-    # TODO: complete and replace the "InsertHereAPydanticClass" with the correct Pydantic classes defined in web_service/lib/models.py
+@app.post("/predict", response_model=PredictionOut, status_code=201)
+def predict(payload: InputData) -> dict:
+    return {"health_check": "Prediction missing!"}
