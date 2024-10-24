45b899534e618e4028c826ff38c4c3a852e58cc3
FrancoisLbrn <francois.lebrun3@gmail.com>
2024-10-24 21:02:17 +0200

    fix: start Prefect server in the background whil FastAPI is running

--- bin/run_services.sh
+++ bin/run_services.sh
@@ -2,10 +2,11 @@ prefect config set PREFECT_API_URL=http://0.0.0.0:4200/api
 
 sqlite3 --version
 
-prefect server start --host 0.0.0.0
+# Start Prefect server in the background
+prefect server start --host 0.0.0.0 &
 
 # Go in the good directory
-cd src/web_service
+cd ../src/web_service
 
-# Lancer l'API FastAPI avec Uvicorn
+# Start FastAPI with Uvicorn
 uvicorn main:app --reload --port 8001
