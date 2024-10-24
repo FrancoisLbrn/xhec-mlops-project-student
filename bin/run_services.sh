6b61ebaeea11bf30453467784ad9e9c44cbb9412
Matthieu Delsart <matthieu.delsart@hec.edu>
2024-10-24 23:03:36 +0200

    fix: run_services.sh to unix

--- bin/run_services.sh
+++ bin/run_services.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Set Prefect API URL to be accessible at 0.0.0.0:4200
-prefect config set PREFECT_API_URL=http://0.0.0.0:4200/api
+prefect config set PREFECT_API_URL=http://127.0.0.1:4200/api
 
 # Start Prefect server in the background
-prefect server start --host 0.0.0.0 --port 4200 &
+prefect server start --host 127.0.0.1 --port 4200 &
 
 # Navigate to the correct directory
 cd /web_service  # Make sure this path is correct
 
 # Start FastAPI with Uvicorn on port 8001
-uvicorn main:app --host 0.0.0.0 --port 8001
+uvicorn main:app --host 127.0.0.1 --port 8001
