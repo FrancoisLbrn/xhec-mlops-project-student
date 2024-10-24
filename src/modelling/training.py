fea03e86ee27fc771c099510336c98dae6c2578f
Matthieu Delsart <matthieu.delsart@hec.edu>
2024-10-24 20:20:19 +0200

    fix: model edit + formatting

--- src/modelling/training.py
+++ src/modelling/training.py
@@ -4,6 +4,6 @@ from sklearn.linear_model import Ridge
 
 def train_model(X: np.ndarray, y: np.ndarray) -> Ridge:
     """Fit a Ridge model on the data."""
-    model = Ridge()
+    model = Ridge(alpha=1.0, fit_intercept=False)
     model.fit(X, y)
     return model
